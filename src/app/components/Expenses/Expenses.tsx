9364df04696dd0d664dda6ea2403daad4a1ba9d7
fatmakhv <fatmaba@gmail.com>
2023-08-20 19:20:03 +0100

    Typofix

--- src/app/components/Expenses/Expenses.tsx
+++ src/app/components/Expenses/Expenses.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState } from 'react';
-import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
